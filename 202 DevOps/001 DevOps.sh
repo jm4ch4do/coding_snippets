b09b28d40e9da27f094033fd4041d1650992fdea
Jose Machado <m4ch4do@protonmail.com>
2022-06-14 15:23:21 -0400

    Notes on Docker Basic concepts and installation

---
+++ 202 DevOps/001 DevOps.sh
@@ -0,0 +1,9 @@
+" ------------------------BASIC------------------------- "
+# DevOps o Site Realiability Engineer
+
+# The basic Docker structure is Linux -> Container Engine -> Docker Container
+# The Engine communicates with the containers using ports instead of IPs.
+
+# After openning Windows Powershell
+docker --version
+docker-compose version
