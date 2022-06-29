970323b46e10cc140d53815c628ff27fe6f46368
Jose Machado <m4ch4do@protonmail.com>
2022-06-29 15:32:42 -0400

    new file for loops and conditionals

--- 301 Java/004 for if.java
+++ 301 Java/004 for if.java
@@ -1,58 +1,3 @@
-"------------------------DATA TYPES-------------------------"
-USED    SIZE        SIZE        PRIM/REF     VALUE  
-*       boolean     1 bit       primitive    true or false   
-        byte        1 byte      primitive    -128 to 127
-        short       2 bytes     primitive    -32 768 to 32,767                 
-*       int         4 bytes     primitive    -2 billion to 2 billion
-        long        8 bytes     primitive    -9 quintillion to 9 quin.
-
-        float       4 bytes     primitive    fractional number up to 6-7 digits
-*       double      8 bytes     primitive    fractional number up to 15 digits
-
-*       char        2 bytes     primitive    single character/letter/ASCII ex. 'f'           
-*       String      varies      reference    a sequence of characters ex. "Hello"  
-    
-// reference data types store an address instead of data
-// the can store more than 1 value
-// but used more memory and are slower that primitive data types
-
-long x = 123456678990045434L;  // assigning long number always with L at the end
-float y = 3.14f;  // f at the end
-double y = 3.14;  // this type is more common    
-boolean z = false;
-
-// Wrapper data types extend the primitive datatypes to be reference
-// and have new useful methods
-
-// primitive    wrapper
-// boolean      Boolean
-// char         Character
-// int          Integer
-// double       Double
-    
-"------------------------VARIABLES-------------------------"
-// dataType variableName
-int x;        // declaration
-x = 123;      // assignment
-int x = 123;  // initialization
-
-
-
-
-"------------------------OPERATORS-------------------------"
-"My number is:" + x;  // string concat 
-+ - * / %
->= <= && || == 
-! // not
-count++ count--
-Math.min(x, y)  Math.max() Math.abs() Math.sqrt() .ceil() .round() .floor()
-    
-import java.util.Random;
-int x = random.nextInt();
-double x = random.nextDouble();
-boolean z = random.nextBoolean()
-    
-    
 "------------------------IF-------------------------"
 public class Main {
     
@@ -138,11 +83,5 @@ String[] animals = {"cat", "dog", "rat", "bird"}
 for(String i : animals){
     System.out.println(i);
 }
-    
-    
-    
-    
-    
-    
     
     
