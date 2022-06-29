1c65cc3bcb4e4f39b358629981174a0a672a7450
Jose Machado <m4ch4do@protonmail.com>
2022-06-29 10:19:09 -0400

    Example for user input

--- 301 Java/001 basic.java
+++ 301 Java/001 basic.java
@@ -28,8 +28,24 @@ public class Main {
         */
     }
 }
+
+
+"------------------------GET USER INPUT-------------------------"
+import java.util.Scanner;
+
+public class Main{
     
-    
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("What is your name? ");
+        String name = scanner.nextLine();
+        
+        System.out.println("Hello "+name);
+        
+    }
+}
     
     
     
