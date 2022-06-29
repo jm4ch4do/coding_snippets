80f7c72e06d43670c3d089698dcd19bb704a084e
Jose Machado <m4ch4do@protonmail.com>
2022-06-29 11:46:30 -0400

    Operators, if, while, for, switch

--- 301 Java/001 basic.java
+++ 301 Java/001 basic.java
@@ -41,13 +41,34 @@ public class Main{
         
         System.out.println("What is your name? ");
         String name = scanner.nextLine();
-        
         System.out.println("Hello "+name);
+        
+        System.out.println("How old are you? ");
+        int age= scanner.nextInt();
+        System.out.println("You are "+age+" years old");
+                
         
     }
 }
     
-    
+
+"------------------------BASIC USER INTERFACE-------------------------"
+import javax.swing.JOptionPane;
+
+public class Main {
+    public static void main(String[] args){
+        
+        String name = JOptionPane.showInputDialog("Enter your name");
+        JOptionPane.showMessageDialog(null, "Hello"+name);
+        
+        int age = Integer.parseDouble(JOptionPane.showInputDialog("Enter your age"));
+        JOptionPane.showMessageDialog(null, "You are "+age+" years old");
+        
+        double height = Double.parseInt(JOptionPane.showInputDialog("Enter your age"));
+        JOptionPane.showMessageDialog(null, "You are "+height+" cm tall")
+        
+    }
+}
     
     
     
--- 301 Java/002 oper_var_func.java
+++ 301 Java/002 oper_var_func.java
@@ -10,7 +10,7 @@ USED    SIZE        SIZE        PRIM/REF     VALUE
 *       double      8 bytes     primitive    fractional number up to 15 digits
 
 *       char        2 bytes     primitive    single character/letter/ASCII ex. 'f'           
-*       string      varies      reference    a sequence of characters ex. "Hello"  
+*       String      varies      reference    a sequence of characters ex. "Hello"  
     
 // reference data types store an address instead of data
 // the can store more than 1 value
@@ -30,11 +30,95 @@ int x = 123;  // initialization
 
 "------------------------OPERATORS-------------------------"
 "My number is:" + x;  // string concat 
++ - * / %
+>= <= && || == 
+! // not
+count++ count--
+Math.min(x, y)  Math.max() Math.abs() Math.sqrt() .ceil() .round() .floor()
+    
+import java.util.Random;
+int x = random.nextInt();
+double x = random.nextDouble();
+boolean z = random.nextBoolean()
 
     
+"------------------------IF-------------------------"
+public class Main {
     
+    public static void main(String[] args){
+        
+    int age = 18;
+        
+    if(age>=18){
+        System.out.println("You are and adult");        
+    }
+    else if(age>=75){
+        System.out.println("Ok Boomer"); 
+    }
+    else{
+        System.out.println("You are not an adult"); 
+    }
+        
+    }
+}
+
+
+"------------------------SWITCH CASE-------------------------"
+public class Main {
     
+    public static void main(String[] args){
+        
+    String day = "Friday";
+        
+    switch(day){
+        case "Sunday": System.out.println("It is Sunday!");
+        break;
+        case "Monday": System.out.println("It is Monday!");
+        break;
+        case "Friday": System.out.println("It is Monday!");
+        break;
+        default: System.out.println("That is not a day!")
+        
+    }
+        
+    }
+}
+
+
+"------------------------WHILE-------------------------"
+// Keep asking for name until user types something
+import java.util.Scanner;
+
+public class Main {
     
+    public static void main(String[] args){
+        
+        Scanner scanner = new Scanner(System.in);
+        String name = "";
+        
+        while(name.isBlank()){
+            System.out.print("Enter your name: ");
+            name = scanner.nextLine();
+        }
+        
+        System.out.println("Hello"+name);
+    }
+    
+}
+
+// do loop (always performs code at least once)
+        do{
+            System.out.print("Enter your name: ");
+            name = scanner.nextLine();
+        } while(name.isBlank());
+    
+
+"------------------------FOR-------------------------"
+for(int i=10; i>=0; i--){    // i-=2 to decrement 2
+    System.out.println(i);
+}
+
+System.out.println("Happy new year!");
     
     
     
