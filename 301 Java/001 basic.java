6953c1290f841c6bc735e56e86e1c47746c987a6
Jose Machado <m4ch4do@protonmail.com>
2022-06-29 09:51:44 -0400

    Basics and Data Types

---
+++ 301 Java/001 basic.java
@@ -0,0 +1,39 @@
+"------------------------BASIC-------------------------"
+// Top 3 languages
+// Flexible
+
+    
+"------------------------COMPILING-------------------------"
+// Compiling transform source code into machine code
+// If you compile on PC the code will only work on PC
+// Java's solution for this is an intermediate step called byte code
+// That has the .class extension and is cross-platform
+// JVM handles the transformation of the byte code to machine code
+JDK - Java Development Kit
+    JRE - Java Runtime Env
+    JVM - Java Virtual Machine
+    
+    
+"------------------------SIMPLE APP-------------------------"
+// You always need a Main class and a main Method within
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!\\");
+        System.out.print("\tHello \"world\"!\n");
+        
+        // Single line notes
+        
+        /* 
+            Multi-line Comments
+        */
+    }
+}
+    
+    
+    
+    
+    
+    
+    
+    
+    
