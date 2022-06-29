cf5894ed7de491b2adbd220aada2209d4ec8f109
Jose Machado <m4ch4do@protonmail.com>
2022-06-29 11:58:44 -0400

    Arrays and 2D arrays

---
+++ 301 Java/003 array.java
@@ -0,0 +1,33 @@
+"------------------------ARRAY-------------------------"
+ // Accesing a value
+String[] cars = {"Camaro", "Corvette", "Tesla"};
+cars[0]
+
+// ini a value
+String[] cars = new String[3];
+car[0] = "Camaro";
+car[1] = "Corvette";
+car[2] = "Tesla";
+
+// print an array
+for(int i=0; i<cars.length; i++){
+    System.out.println(cars[i]);
+}
+
+
+"------------------------2D ARRAY-------------------------"
+String[][] cars = new String[2][2];
+cars[0][0] = "Camaro";
+
+//you can also create it this way
+
+String[][] cars = {
+    {"Camaro", "Corvette", "Silverado"},
+    {"Mustang", "Ranger", "F-150"}
+}
+
+for(int j=0; j<cars[i].lenght; j++){
+    System.out.print(cars[i][j]+"");
+}
+    
+    
