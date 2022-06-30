5fdb305e92146f1ddc7534494ed5cdad33f8ee79
Jose Machado <m4ch4do@protonmail.com>
2022-06-30 09:52:42 -0400

    Add abstract class example

--- 301 Java/005 OOP.java
+++ 301 Java/005 OOP.java
@@ -82,3 +82,42 @@ public class Worker extends Human{
     }
 }
 
+
+"------------------------ABSTRACT CLASS-------------------------"
+// Abstract classes cannot be instanciated but they can be extended
+// So they are often used as a template for other objects
+// Abstract class
+abstract class Animal {
+  public abstract void animalSound();   // Abstract method (does not have a body)
+  // Regular method
+  public void sleep() {
+    System.out.println("Zzz");
+  }
+}
+
+// Subclass (inherit from Animal)
+class Pig extends Animal {
+  public void animalSound() {
+    // The body of animalSound() is provided here
+    System.out.println("The pig says: wee wee");
+  }
+}
+
+class Main {
+  public static void main(String[] args) {
+    Pig myPig = new Pig(); // Create a Pig object
+    myPig.animalSound();
+    myPig.sleep();
+  }
+}
+
+
+
+
+
+
+
+
+
+
+    
