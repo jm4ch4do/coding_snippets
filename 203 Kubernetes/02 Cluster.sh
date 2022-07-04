d371c6d48fd898460735911ef3258a39b39e06a1
Jose Machado <m4ch4do@protonmail.com>
2022-07-04 13:10:16 -0400

    Temporary alias for kubectl

--- 203 Kubernetes/02 Cluster.sh
+++ 203 Kubernetes/02 Cluster.sh
@@ -10,11 +10,18 @@ kubectl get nodes  # shows list of nodes
 kubectl get pods   # shows list of pods
 
 
-"-----------------------------NAMESPACES-----------------------------"
-# Namespaces are used to separate pods
+"--------------------------NAMESPACES & PODS-------------------------"
+# Namespaces are used to separate pods (new pods go to default namespace)
 kubectl get namespaces  # show list of namespaces
 kubectl get pods --namespace=kube-system  # shows internal containers used by docker
+                                          # which are stored in the kube-system namespace
+
+# new pod which a docker image from dockerhub
+kubectl run nginx --image=nginx
+
+# details on pod including the IP address
+kubectl describe pod nginx
 
 
-
-
+"---------------------------------ALIAS--------------------------------"
+alias k="kubectl"  # replacing the long command kubectl by k (temporary alias)
