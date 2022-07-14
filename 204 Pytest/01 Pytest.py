280562ea555c398f7b945e198528f728b4e31e7d
Jose Machado <m4ch4do@protonmail.com>
2022-07-14 07:52:43 -0400

    File Structure and First Test

--- 204 Pytest/01 Pytest.py
+++ 204 Pytest/01 Pytest.py
@@ -1,11 +1,33 @@
-''' ------------------------------- PYTEST -----------------------------  '''
+''' ------------------------------- BASIC -----------------------------  '''
 # install
 pip install pytest-django
 
 # docs
 https://pytest-django.readthedocs.io/en/latest/
+    
+# youtube video
+https://www.youtube.com/watch?v=LYX6nlECcro&list=PLOLrQ9Pn6caw3ilqDR8_qezp76QuEOlHY
 
+    
+''' --------------------------- FILE STRUCTURE ------------------------- '''
+# create a folder and a file outside the project folder (file is outside the folder)
+mkdir test
+nano pytest.ini
+    [pytest]
+    DJANGO_SETTINGS_MODULE = project_lease.settings  
 
+    # naming conventions for test files
+    python_files = test.py test_*.py *_test.py
+
+    
+''' ------------------------------ FIRST TEST --------------------------- '''
+# create your first simple test
+nano test_ex1.py
+import pytest
+def test_example():
+    assert 1 == 1
+    
+command_lines >> pytest
 
 
 
