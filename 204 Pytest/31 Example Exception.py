d305b5a84eac8ecacd51239197c9531c52f84aa0
Jose Machado <m4ch4do@protonmail.com>
2022-07-15 22:14:20 -0400

    Add extra exception example

--- 204 Pytest/31 Example Exception.py
+++ 204 Pytest/31 Example Exception.py
@@ -5,6 +5,10 @@
 
 class Point():
     def __init__(self, name, latitude, longitude):
+        
+        if not isinstance(name, str):
+            raise ValueError("Invalid city name, city name must be a string.")
+        
         self.name = name
 
         if not (-90 <= latitude <= 90) or not (-180 <= longitude <= 180):
@@ -23,13 +27,20 @@ import pytest
 
 from scripts.chp2.video3.mapmaker_exceptions_end import Point
 
-
+# test data being created correctly
 def test_make_one_point():
     p1 = Point("Dakar", 14.7167, 17.4677)
     assert p1.get_lat_long() == (14.7167, 17.4677)
 
-
+# test wrong latitud or longitude triggers an error
 def test_invalid_point_generation():
     with pytest.raises(ValueError) as exp:
         Point("Senegal", 99.6937, -189.44406)
     assert str(exp.value) == "Invalid latitude, longitude combination."
+    
+# test wrong city name triggers an error
+def test_invalid_name_generation():
+    with pytest.raises(ValueError) as exp:
+        Point(10, 50, 50)
+    # breakpoint()
+    assert str(exp.value) == "Invalid city name, city name must be a string."
