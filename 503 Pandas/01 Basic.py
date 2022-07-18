80d743a1b11f9d111fced224d26a70203b7daba9
Jose Machado <m4ch4do@protonmail.com>
2022-07-17 21:37:06 -0400

    Basic concepts

---
+++ 503 Pandas/01 Basic.py
@@ -0,0 +1,18 @@
+''' ------------------- SOURCE ---------------- '''
+# https://www.linkedin.com/learning/pandas-essential-training/
+
+
+''' ------------------- INSTALL ---------------- '''
+pip install pandas
+import pandas as pd
+
+
+''' ------------------- VERSIONS ---------------- '''
+pd.show_versions()  # shows pandas version and also
+                    # the version of every pandas dependency
+    
+pd.__version__  # show only pandas version
+
+
+''' ------------------- CONCEPT ---------------- '''
+# Pandas provides easy to use data analysis tools
