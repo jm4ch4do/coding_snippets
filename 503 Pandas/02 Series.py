f0431d79ce3b7899614ce92e0f4a6237a968a1d4
Jose Machado <m4ch4do@protonmail.com>
2022-07-17 21:37:20 -0400

    Dataframe and series

---
+++ 503 Pandas/02 Series.py
@@ -0,0 +1,33 @@
+''' ------------------- CONCEPT ---------------- '''
+# Dataframe in pandas is a table
+# Series is a field
+# The first column is usually the index
+
+# Three different ways of accesing a series
+DataFrame['seriesName']
+DataFrame["seriesName"]
+DataFrame.SeriesName  # doesn't work is there is a space in the serie's name
+
+# Access multiple series
+DataFrame[['SeriesName1', 'SeriesName2']]
+
+# identify type of object (Dataframe or Series)
+type(DataFrame)
+# if you selected more than one series then the object type is dataframe
+
+
+''' ------------------- READ FILE ---------------- '''
+file_path = './data/olympics.csv'
+oo = pd.read_csv(file_path, skiprows=4)  # read file
+
+
+''' -------------- DISPLAY DATAFRAME ------------- '''
+oo.head()  # see first 5 lines of data frame
+oo  # display first 5 and last 5 lines of data frame
+
+
+''' -------------- DISPLAY SERIES ------------- '''
+OO['city']
+
+# dataframe with 4 series
+oo[['Edition', 'City', 'Athlete', 'Medal']]
