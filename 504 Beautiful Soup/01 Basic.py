b0076d7d2d52a7d0d1c9a7ba2ec9456c43b2cd17
Jose Machado <m4ch4do@protonmail.com>
2022-07-18 15:37:13 -0400

    Basic info on Web Scrapping

---
+++ 504 Beautiful Soup/01 Basic.py
@@ -0,0 +1,20 @@
+''' ------------------- SOURCE ---------------- '''
+# https://www.linkedin.com/learning/using-python-for-automation/the-value-of-web-scraping
+
+
+''' ------------------- CONCEPT ---------------- '''
+# Beautiful Soup is for Web Scrapping which has three steps
+# 1. Send Get request to page that returns and html document   
+# 2. Parse the HTML doc to make it easier to read
+# 3. Extract data
+
+
+''' ------------------- INSTALL ---------------- '''
+pip install request  # get the get query
+pip install lxml  # processing the html
+pip install beautifulsoup4  # create a parse a navigable html doc
+
+
+''' ------------------- TESTING ---------------- '''
+# This web is always available for scrapping
+https://quotes.toscrape.com/
