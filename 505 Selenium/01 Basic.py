d08fc178c97201ecb3f273f1f2ccd9acced47309
Jose Machado <m4ch4do@protonmail.com>
2022-07-18 17:10:55 -0400

    Minor fix

---
+++ 505 Selenium/01 Basic.py
@@ -0,0 +1,23 @@
+''' ------------------- SOURCE ---------------- '''
+# https://www.linkedin.com/learning/using-python-for-automation/
+
+
+''' ------------------- CONCEPT ---------------- '''
+# Beautiful Soup is for Web Scrapping which has three steps
+# 1. Send Get request to page that returns and html document   
+# 2. Parse the HTML doc to make it easier to read
+# 3. Extract data
+
+
+''' ------------------- INSTALL ---------------- '''
+pip install request  # get the get query
+pip install lxml  # processing the html
+pip install beautifulsoup4  # create a parse a navigable html doc
+
+
+''' ------------------- TESTING ---------------- '''
+# This web is always available for scrapping
+https://quotes.toscrape.com/
+    
+# Extra docs about scrapping
+https://scrapingclub.com
