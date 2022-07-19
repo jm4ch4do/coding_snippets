a4664a3dfd5aeca77a18b3430612f01cc5c3b058
Jose Machado <m4ch4do@protonmail.com>
2022-07-19 15:40:02 -0400

    Example with API keys

--- 506 API/01 Basic.py
+++ 506 API/01 Basic.py
@@ -9,6 +9,9 @@
 # www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita
 # or any other in this list
 # https://mixedanalytics.com/blog/list-actually-free-open-no-auth-needed-apis/
+# here you can find many APIs for software developers
+# https://rapidapi.com/
+
 
 ''' ------------------- INSTALL ---------------- '''
 pip install requests  # get the get query
