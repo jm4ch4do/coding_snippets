8e807446cc032c9ce65767b713ce81f1a79a5690
Jose Machado <m4ch4do@protonmail.com>
2022-07-21 20:36:21 -0400

    info on Pydoc and help

---
+++ 002 python/examples/888 docs.py
@@ -0,0 +1,8 @@
+''' ---------------------------- PYDOC --------------------------  '''
+>> python -m pydoc collections.namedtuple  # docs for this package
+
+
+from collections import namedtuple
+print(namedtuple.__doc__)
+#or
+print(help(namedtuple))
