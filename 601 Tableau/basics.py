b25457956437ce0d40bea2b19bed76c27e62a658
Jose Machado <m4ch4do@protonmail.com>
2022-08-04 09:33:06 -0400

    Basic about Tableau

---
+++ 601 Tableau/basics.py
@@ -0,0 +1,12 @@
+''' ------------------- TABLEAU FILE TYPES ---------------- '''
+.twb   # tableau workbook
+       # store visualization without source data
+    
+.tds   # stores the server address, password, and other information
+       # required to access a data source
+    
+.tbm   # Tableau bookmark stores a connection to a worksheet in another Tableau workbook
+
+.tde   # stores Tableau data as a filtered and aggregated extract
+
+.twbx  # stores extracted data and visualizations for viewing in Tableau our Tableau Reader
