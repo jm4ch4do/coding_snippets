ad3a41ebbc4ad235f2d96ab3a32e0f004499fa98
Jose Machado <m4ch4do@protonmail.com>
2022-08-24 15:43:00 -0400

    Add shared responsability model.

---
+++ 701 Azure/01 Azure Cloud.py
@@ -0,0 +1,26 @@
+''' -------------------- AZURE FUNDAMENTALS -------------------- '''
+# Azure Fundamentals Covers 3 Topics
+1. Describe Cloud Concepts
+2. Describe Azure Architecture and Services
+3. Describe Azure Management and governance
+
+
+
+''' -------------------- AZURE CLOUD COMPUTING -------------------- '''
+# With cloud computing you purchase the resources you need in a cloud data center
+# instead of your own personal computer, so you can easily adjust the services you need
+# and delegate maintenance tasks to the cloud provider.
+
+Basic Services: Compute Power, Storage
+
+
+
+
+
+# Objectives
+Define cloud computing.
+Describe the shared responsibility model.
+Define cloud models, including public, private, and hybrid.
+Identify appropriate use cases for each cloud model.
+Describe the consumption-based model.
+Compare cloud pricing models.
