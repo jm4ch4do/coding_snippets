659e7213672e72e666a360d120fea3bb10c05c99
Jose Machado <m4ch4do@protonmail.com>
2022-10-18 13:31:33 -0400

    Basic definitions

---
+++ 003a Flask/basic.py
@@ -0,0 +1,5 @@
+' ---------------------------- BASICS ----------------------------- '
+1. Flask Backend
+2. Jinja Template Engine
+3. Werkzeug WSGI Toolkit  - 
+   WSGI is the Web Server Gateway Interface. It is a specification that describes how a web server communicates with web applications, and how web applications can be chained together to process one request.
