4faaff0e3bdd0c3310ffeeb70c744d736683d169
Jose Machado <m4ch4do@protonmail.com>
2022-11-15 15:43:47 -0500

    Add exercise files

---
+++ 206 Terraform/01 Basics.sh
@@ -0,0 +1,25 @@
+"------------------------BASIC-------------------------"
+# Infrastructure as code (IaC): Provisioning infrastructure through
+# software to achiveve consistent and predictable deployments
+
+# Terraforms allows automating the deployment of infrastructure in 
+# multiple providers both from public and private cloud
+
+# Terraform uses declarative approach, meaning it defines JSONs and 
+# other data structures to declare what it wants to do, instead of
+# using function and parameters which will be an imperative approach
+# Terraform uses HCL (HashiCorp Configuration Languague) that is very
+# similar to JSON, or it can also use pure JSON
+
+# Terraform is Idempotent: If you make changes for your environment
+# once it will work but if you try again it will let you know that
+# this changes already occurred so is like Terraform has some kind
+# of intelligence to avoid making mistakes
+
+# Why use Terraform:
+# Automated Deployement
+# Repeatable Process
+# Consistent environments
+# Reusable components
+# Documented architecture
+
