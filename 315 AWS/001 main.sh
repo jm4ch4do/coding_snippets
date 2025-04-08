4246492bd8a1061482cd372d78f2a70fb77d206a
jose <m4ch4do@protonmail.com>
2025-04-08 11:25:21 -0400

    doc: introduces aws basics and ec2 instances

---
+++ 315 AWS/001 main.sh
@@ -0,0 +1,17 @@
+# ----------------------------------- USER ACCESS -----------------------------------
+IAM (Identitiy and Access Management)  # global service (doesn't depend on region)
+Users  # One root user, the rest are IAM users with limited permissions
+Groups  # users can be in multiple groups, groups cannot contain other groups
+Policies  # policies can be assigned to users or groups. You can use pre-defined policies
+          # or create your own assigning permissions of your choice.
+UI, Console, SDK(api)  # are the three ways to access aws services
+Alias  # you can set for any user for easier access
+Roles  # are like groups but they contain permissions for non-user access.
+       # example: you can give an ec2 instance permission to access s3 bucket.
+
+
+
+
+
+S3  # Object storage (files, images, backups) but not applications or OS
+    # Data remains even if EC2 is deleted, not instant access but optimize for large-scale
---
+++ 315 AWS/011 ec2.sh
@@ -0,0 +1,71 @@
+# --------------------------------------- EC2 BASICS ---------------------------------------
+# --- AWS services
+EC2  # Elastic Compute Cloud, provides infrastructure as a service
+EBS (Elastic Block Store)  # Storage volumes for EC2 (like hard drive)
+ELB (Elastic Load Balancer)  # Distributes traffic to EC2s for high availability
+ASG (Auto Scaling Group)  # Automatically scales EC2 instances based on demand
+
+EC2 user data  # root user custom script that runs on creation to make updates, install software or apps
+
+# --- Instance selection
+m5.2xlarge
+m  # instance class - m is general purpose instance, good for web servers or code repos
+   #                    (good balance between compute, memory, networking, consistent performance)
+   #                - t is also general purpose but burstable (peaks of requests like websites)
+   #                - c compute optimized (machine learning, gaming servers, batch processing workloads)
+   #                - r ram performance for large datasets in memory (dbs, web caches, business inteligence)
+   #                - i,d storage optimized (dbs, cache, warehousing, distributed file systems)
+5  # generation of instance - 5th generation
+2xlarge  # size within instance class
+
+# --- Security Groups
+# are firewalls placed on EC2 instances. Rules apply per ip or per another security group.
+# You can enable EC2 that are part of a given sec group to access your EC2.
+# they live outside of the EC2 so if it blocks access, EC2 will not see it. 
+# One EC2 can be associated to multiple security groups (most permissive rule is applied). 
+# Is good to maintain one sec group for ssh access. By default all inbound traffic is blocked and outbound traffic is authorised.
+# When security groups blocks, you'll see a time out. When application blocks you'll see a connection refused.
+# Commonly used ports in Security Groups
+22 - SSH    # log into instance
+21 - FTP    # upload files into large share
+22 - SFTP   # uploads files using SSH
+80 - HTTP   # access unsecured websites
+443 - HTTPS # access secured websites
+3389 - RDP  # remote desktop to windows instance
+
+# --- Reserved Instances
+On-Demand Instances  # this are the regular ones, you pay by second
+Reserved (1 & 3 years)  # long workloads, specific instance type, region, and OS. 72% discount.
+Savings Plans (1 & 3 years)  # long workloads, free to change specifics. 66% discount
+Spot Instances  # short workload, instance may be terminated by AWS. 90% discount
+Dedicated Hosts (on demand or reserved)  # book an entire physical server, control instance placement (most expensive)
+Dedicated Instances  # no other customer will share your hardware but may share hardware with other instances in same account.
+Capacity Reservations  # reserve capacity in a specific AZ for any duration. Pay same on-demand fees but you guarantee you'll always have availavility.
+
+# --------------------------------------- AMI ---------------------------------------
+AMI  # Amazon Machine Image. Customization of an EC2 instance. 
+     # You can create an AMI from any EC2 instance.
+EC2 Image Builder  # automates image build (to a schedule or to when packages are updated)
+                   # it includes creation and instanciation
+
+# --------------------------------------- EC2 STORAGE ---------------------------------------
+EBS  # Elastic Block Store. Default storage choice in UI. (kind of a "network USB stick") 
+     # Is a network drive that can be attached/dettached and persisted independently. 
+     # So, information can stay in the drive even after vm is terminated. (Detele on Termination=False) 
+     # can only be mounted at one instance at a time and is bound to specific availability zone.
+     
+Snapshot  # Then, to move volumes accross you need to do snapshots.
+          # Snapshoot Archive is 75% cheaper but it can take 24-72 hours to restore.
+Recycle Bin  # is a way to protect resource for accidental deletion. A retention rule can be set.
+
+EC2 Instance Store  # high performance hardware disk (faster than EBS)
+                    # when instance is terminated data is lost (ephemeral)
+
+EFS # Elastic File System. Network file system that can be mounted to 100s of EC2s
+    # Only for Linux-EC2 and multi-AZ
+EFS Infrequent Access  # cost-optimized for files that are not accessed every day (92% cheaper)
+                       # moving files can be done with a lifecycle policy
+
+FSs  # 3rd party file systems on AWS
+     # FSx for Windows File Server
+     # FSx for Lustre (Linux + cluster). High performance computing like machine learning
