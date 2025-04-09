92f5804900053c17c2f8857c0192178126e9720b
jose <m4ch4do@protonmail.com>
2025-04-09 15:10:01 -0400

    doc: adds artifacts, outputs and caching

--- 316 Actions/001 main.sh
+++ 316 Actions/001 main.sh
@@ -64,6 +64,60 @@ jobs:
     needs: test  # also takes a list: [test, job2]
 
 
+# -------------------------------------- ACTIONS --------------------------------------
+# you can use your own actions or use community actions from marketplace
+      - name: Install NodeJS
+        uses: actions/setup-node@v3
+        with:
+          node-version: 18  # parameters are specified in the action documentation
+
+# --- ARTIFACTS
+# actions that allows generating files and folders (typically used to store or analyze)
+      - name: Upload artifacts
+        uses: actions/upload-artifact@v4.6.2
+        with:
+            name: dist-files  # takes everything in this folder
+            path: dist  # and puts it here
+
+# files are not automatically available in other jobs, so to import them:
+      - name: Get build artifacts
+        uses: actions/download-artifact@v4
+        with:
+            name: dist-files
+
+# --- OUTPUTS
+# actions that output simple values instead of files
+    outputs:
+        script-file: ${{ steps.publish.outputs.script-file}}
+    steps:
+      - name: Publish JS filename
+        id: publish
+        run: find dist/assets/*.js -type f -execdir echo 'script-file={}' >> $GITHUB_OUTPUT ';'
+
+        # alternative syntax
+        run: find dist/assets/*.js -type f -execdir echo '::set-output name=script-file::{}' ';'
+
+# to use in a different file
+      - name: Output file name
+        run: echo ${{ needs.build.outputs.script-file }}
+
+# --- CACHING DEPENDENCIES
+# Installing dependencies is a common step in multiple jobs so is good to cache it
+# the first time it runs so in the next job time spent is reduced
+# hashFiles function is used to build the name using a hash of the content of the file.
+# So, when one of the value in the files change (file holds dependencies)
+# The name will change and github will know that the cache needs to be rebuilt
+      - name: Cache dependencies
+        uses: actions/cache@v4
+        with:
+            path: ~/.npm  # path that should be cached
+            key: deps-node-modules-${{ hashFiles('**/package-lock.json')}}  # name of cache
+      - name: Install dependencies
+        run: npm ci
+
+# this same lines are to be repeated in the second job. Cache is shared among all jobs
+
+
 # -------------------------------------- CONTEXT --------------------------------------
 # github handles variables like secrets in its context which you can visualize using
 # the 'github' value refers here to a given context but you can find more available context at:
