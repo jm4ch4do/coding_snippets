b5578ba2da46934e0956e874957abb3e6fd9f582
jose <m4ch4do@protonmail.com>
2025-07-21 18:24:56 -0400

    doc: adds machine learning for AWS

---
+++ 315 AWS/021 machine.sh
@@ -0,0 +1,37 @@
+# ------------------------------------------- MACHINE LEARNING ---------------------------------------
+Rekognition                 # Finds objects, text, scenes in images and videos. Facial Analysis and search.
+                            # use cases: labeling, content moderation, text detection, face detection.
+Transcribe                  # Converts speech to text. Uses automatic speech recognition (ASR).
+                            # You can remove PII(Personally Identifiable Information). Auto-detects language.
+Polly                       # Turns text into speech.
+Translate                   # Language translation.
+
+Amazon Lex                  # Tech that powers Alexa. Helps build chatbots, call center bots, etc.
+Amazon Connect              # Visual contact center to receive calls, create contact flows. No upfront payments.
+
+Comprehend                  # Natural Language Processing (NLP). Managed and serverless. It can understand language.
+                            # Extracts key phrases, places, people, brands or events. Understand positive or negative parts of the text.
+Kendra                      # Extracts answers from within a document (PDF, HTML, Word ...)
+                            # Learns from user interactions/feedback to promote prefered results.
+
+Personalize                 # Does personalized recommendations. Used by amazon.com to recommend products.
+
+SageMaker                   # Fully managed service to build ML models
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
